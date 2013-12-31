28ca32d9fee90819e5d43854be60425751507522
Jason Merino <jasonmerino@gmail.com>
2013-12-31 10:10:55 -0800

    Moved some files around and modified "make" file.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 ############################
 # .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# This script creates symlinks from the home directory to any desired dotfiles in ~/code/dotfiles
 ############################
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
+# dotfiles directory
+dir=~/code/dotfiles/src
+
+# old dotfiles backup directory
+olddir=~/code/dotfiles/dotfiles_old
+
+# list of files/folders to symlink in src dir
+files=".gitconfig .gitignore .vimrc"
 
 ##########
 
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
+# create dotfiles_old in dotfiles repo dir
 mkdir -p $olddir
-echo "...done"
 
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
+# change to the src dir
 cd $dir
-echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+  mv ~/$file $olddir
+  ln -s $dir/$file ~/$file
 done
