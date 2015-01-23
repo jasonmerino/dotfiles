7936592f09d0f23dc1ba57b5915cdf3b2761c370
Jason Merino <jasonmerino@gmail.com>
2015-01-23 12:23:12 -0800

    Adds better symlinking.

--- Sublime Text 3/link.sh
+++ Sublime Text 3/link.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-ln -s ./SublimeLinter.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/SublimeLinter.sublime-settings
-ln -s ./console-log.sublime-snippet ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/console-log.sublime-snippet
+echo "Linking snippets..."
+ln -s "$PWD/console-log.sublime-snippet" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/console-log.sublime-snippet"
 ln -s "$PWD/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
+
+echo "Linking packages..."
+# ln -s "$PWD/GitGutter.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/GitGutter.sublime-settings"
+ln -s "$PWD/Package Control.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Package Control.sublime-settings"
+ln -s "$PWD/SublimeLinter.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/SublimeLinter.sublime-settings"
