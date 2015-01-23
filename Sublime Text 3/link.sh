4a8f59f68d9e3a31c3e829eaf5f6a9cc161c9df0
Jason Merino <jasonmerino@gmail.com>
2015-01-23 12:15:20 -0800

    Adds sublime preferences as symlink.

--- Sublime Text 3/link.sh
+++ Sublime Text 3/link.sh
@@ -2,4 +2,4 @@
 
 ln -s ./SublimeLinter.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/SublimeLinter.sublime-settings
 ln -s ./console-log.sublime-snippet ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/console-log.sublime-snippet
-cp ./Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+ln -s "$PWD/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
