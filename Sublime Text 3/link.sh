b3e1aafcd1ba94f1e344d0373ccb9988f50dc9ae
Jason Merino <jasonmerino@gmail.com>
2015-02-16 09:35:13 -0800

    Adds keymap symnlink.

--- Sublime Text 3/link.sh
+++ Sublime Text 3/link.sh
@@ -6,8 +6,9 @@ ln -s "$PWD/snippet-context-dump.sublime-snippet" "$HOME/Library/Application Sup
 ln -s "$PWD/snippet-it.sublime-snippet" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/"
 ln -s "$PWD/snippet-desc.sublime-snippet" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/"
 
-
+echo "Linking settings..."
 ln -s "$PWD/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/"
+ln -s "$PWD/Default (OSX).sublime-keymap" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/"
 
 echo "Linking packages..."
 # ln -s "$PWD/GitGutter.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/GitGutter.sublime-settings"
